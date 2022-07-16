74131c2f836f46b126a29202c17d47c031749c7c
Sahan Nagodavithana <sahan.nagodavithana@arimaclanka.com>
2022-07-16 10:18:03 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Sahan Nagodavithana on 2022-07-16.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

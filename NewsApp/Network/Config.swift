b41ac70084844524675a9b47fe90475009cb2e65
Sahan Nagodavithana <sahan.nagodavithana@arimaclanka.com>
2022-07-17 09:31:59 +0530

    API key changed

--- NewsApp/Network/Config.swift
+++ NewsApp/Network/Config.swift
@@ -11,7 +11,7 @@ public struct Config {
     struct API {
         enum STAGING {
             static let HOST = "https://newsapi.org/v2/"
-            static let APIKEY = "4e4660b25ada49318c3a14ab299092bc"
+            static let APIKEY = "e228d28e18d14dc9833911ef6abcabf5"
         }
         enum LIVE {
             static let HOST = "https://newsapi.org/v2/"
